905d9accfa3ffc288034543a806b341955d4e043
Reginald <reginald2197@gmail.com>
2025-09-13 20:39:19 +0800

    test: add login spec

---
+++ tests/features/login.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('Should successfully login', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.getByText('Swag Labs')).toBeVisible();
+});
